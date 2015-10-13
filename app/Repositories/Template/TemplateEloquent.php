3213d19213d921e84527350131a700e0e283708e
trieunb <trieunb08@gmail.com>
2015-10-13 12:28:56 +0700

    fix code

--- app/Repositories/Template/TemplateEloquent.php
+++ app/Repositories/Template/TemplateEloquent.php
@@ -3,16 +3,30 @@ namespace App\Repositories\Template;
 
 use App\Repositories\AbstractRepository;
 use App\Repositories\Template\TemplateInterface;
-use App\Template;
+use App\Models\Template;
 
 class TemplateEloquent extends AbstractRepository implements TemplateInterface
 {
 	protected $model;
 
-	public function __construct(Template $model)
+	public function __construct(Template $template)
 	{
-		$this->model = $model;
+		$this->model = $template;
 	}
-
+    /**
+     * create 
+     * @param $date
+     * @return mixed
+     */
+    public function saveTemplate($data)
+    {
+        // $template = $data['id'] ? 
+        // $this->getById($data['id']) : 
+        // new Template;
+        $template->user_id = $data['user_id'];
+        $template->name = $data['name'];
+        $template->template $data['template'];
+        return $template->save();
+    }
 
 }
