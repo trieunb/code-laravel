215ebfa97a64907a81ce060b7233d63c6e7c82b3
nguyenphuocnhatthanh <nguyenphuocnhatthanh@gmail.com>
2015-10-15 09:03:35 +0700

    change soft skill user

--- config/soft-skill.php
+++ config/soft-skill.php
@@ -2,9 +2,9 @@
 
 return [
 	'question' => [
-		['question_1' => 1], 
-		['question_2' => 2], 
-		['question_3' =>3],
-		['question_4' =>4]
+		['question' => "text question", 'value' => 1], 
+		['question' => "text question", 'value' => 1], 
+		['question' => "text question", 'value' => 1], 
+		['question' => "text question", 'value' => 1]
 	]
 ];
