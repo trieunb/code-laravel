119e0f252b510b7cd21dc19baf3cc510bbd144c5
nguyenphuocnhatthanh <nguyenphuocnhatthanh@gmail.com>
2015-10-16 14:21:25 +0700

    add validate rules

--- app/ValidatorApi/UserSkill_Rule.php
+++ app/ValidatorApi/UserSkill_Rule.php
@@ -6,7 +6,9 @@ class UserSkill_Rule extends AbstractValidator
 	public function rules($params)
 	{
 		return [
-			'skill_name' => 'required'
+			'skill_name' => 'required|min:3',
+			'skill_test' => 'required|integer',
+			'experience' => 'required|min:3'
 		];
 	}
 }
