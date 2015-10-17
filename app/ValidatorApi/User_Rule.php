78a73d1720f82ec23710dc611a5bc8b76522c3cc
nguyenphuocnhatthanh <nguyenphuocnhatthanh@gmail.com>
2015-10-17 11:38:32 +0700

    fix validate user

--- app/ValidatorApi/User_Rule.php
+++ app/ValidatorApi/User_Rule.php
@@ -6,17 +6,27 @@ class User_Rule extends AbstractValidator
 {
 	protected function rules($params)
 	{
-		return [
-		'firstname' => 'required',
-            'lastname' => 'required',
-            'email' => 'required|email|unique:users,email,'.$params,
-            'mobile_phone' => 'integer|min:9|max:11',
-            'home_phone' => 'integer|min:9|max:11',
-            'dob' => 'required',
-            'gender' => 'required',
-            'city' => 'required', 
-            'state' => 'required',
-            'country' => 'required'
-		];
+            $rules = [];
+            if ($this->has('firstname'))
+                  $rules['firstname'] = 'required|min:3';
+            if ($this->has('lastname'))
+                  $rules['lastname'] = 'required|min:3';
+            if ($this->has('email'))
+                  $rules['email'] = 'required|email|unique:users,email,'.$params;
+            if ($this->has('mobile_phone')) 
+                  $rules['mobile_phone'] = 'integer|min:9|max:11';
+            if ($this->has('home_phone'))
+                  $rules['home_phone'] = 'integer|min:9|max:11';
+            if ($this->has('dob'))
+                  $rules['dob'] = 'required';
+            if ($this->has('gender'))
+                  $rules['gender'] = 'required';
+            if ($this->has('state'))
+                  $rules['state'] = 'required|min:3';
+            if ($this->has('city'))
+                  $rules['city'] = 'required|min:3';
+            if ($this->has('country'))
+                  $rules['country'] = 'required|min:3';
+		return $rules;
 	}
 }
