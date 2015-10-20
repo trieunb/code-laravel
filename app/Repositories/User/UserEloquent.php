0be07590d046c2fcd28876c0eadda939727867f6
nguyenphuocnhatthanh <nguyenphuocnhatthanh@gmail.com>
2015-10-20 15:43:02 +0700

    update code

--- app/Repositories/User/UserEloquent.php
+++ app/Repositories/User/UserEloquent.php
@@ -25,6 +25,7 @@ class UserEloquent extends AbstractRepository implements UserInterface
 	public function saveFromApi($data, $user_id = null)
 	{
 		$user =  $this->getById($user_id);
+
 		if (isset($data['firstname']))
 			$user->firstname = $data['firstname'];
 		if (isset($data['lastname']))
