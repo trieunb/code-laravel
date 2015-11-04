b8e706b8fcea1731eaa89051cb34bd65ae754efa
trieunb <trieunb08@gmail.com>
2015-11-04 08:32:19 +0700

    update code

--- resources/views/frontend/template/basic_template.blade.php
+++ resources/views/frontend/template/basic_template.blade.php
@@ -10,23 +10,21 @@
         <link rel="stylesheet" href="{{ asset('css/fonts.css') }}" media="screen" title="no title" charset="utf-8">
         <link rel="stylesheet" href="{{ asset('css/style.css') }}" media="screen" title="no title" charset="utf-8">
         <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
-         <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
+        <link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
     </head>
     <body>
         <div class="container">
             <div class="row">
-                <div class="header">
-                    <div class="image-avatar">
-                    @if ( !empty($template->avatar['origin'] ))
-                        <img style="width:100%;" id="image" src="{{ asset($template->avatar['origin']) }}">
-                    @else
-                        <img style="width:100%;" id="image" src="{{ asset('images/avatar.jpg') }}">
-                    @endif
-                    <div class="text-info text-center">
+                <div class="image-avatar">
+                @if ( !empty($template->avatar['origin'] ))
+                    <img style="width:100%;" id="image" src="{{ asset($template->avatar['origin']) }}">
+                @else
+                    <img style="width:100%;" id="image" src="{{ asset('images/avatar.jpg') }}">
+                @endif
+                    <div class="text-infomation text-center">
                         <p class=full-name>{!! $template->firstname . ' ' . $template->lastname !!}</p>
                         <span>{{ $template->link_profile }}</span><br>
                         <span>{{ $template->email }}</span>
-                    </div>
                     </div>
                 </div>
                 <div class="info text-center">
@@ -34,56 +32,54 @@
                     <span>{!! $template->city . ',' . $template->state !!}</span><br>
                     <span>Tell: {!! $template->mobile_phone !!}</span>
                 </div>
-                <div class="content">
-                    <div class="content-box">
-                        <div class="header-title">
-                            <span>Infomation</span>
-                        </div>
-                        <div class="box">
-                            <span>{!! $template->infomation !!}</span>
-                        </div>
+                <div class="content-box">
+                    <div class="header-title">
+                        <span>Infomation</span>
                     </div>
-                    <div class="content-box">
-                        <div class="header-title">
-                            <span>Education</span>
-                        </div>
-                        <div class="box">
-                            <label>School Name:</label>
-                            @foreach($template->user_educations as $education)
-                            <span>
-                                {!! $education['school_name'] !!}  
-                            </span> ,
-                            @endforeach
-                        </div>
+                    <div class="box">
+                        <span>{!! $template->infomation !!}</span>
                     </div>
-                    <div class="content-box">
-                        <div class="header-title">
-                            <span>Work Experience</span>
-                        </div>
-                        <div class="box">
-                            <label>Company:</label>
-                            @foreach($template->user_work_histories as $work)
-                            <span>
-                                {!! $work['company'] !!}
-                            </span>,
-                            @endforeach
-                        </div>
+                </div>
+                <div class="content-box">
+                    <div class="header-title">
+                        <span>Education</span>
                     </div>
-                    <div class="content-box">
-                        <div class="header-title">
-                            <span>Skills</span>
-                        </div>
-                        <div class="box">
-                            @foreach($template->soft_skill as $skill)
-                            <span>
-                                {!! $skill['question'] !!}
-                            </span> ,
-                            @endforeach
-                            <p>
-                              <label for="amount">Text Size:</label>
-                            </p>
-                            <div id="slider-range-min"></div>
-                        </div>
+                    <div class="box">
+                        <label>School Name:</label>
+                        @foreach($template->user_educations as $education)
+                        <span>
+                            {!! $education['school_name'] !!}  
+                        </span> ,
+                        @endforeach
+                    </div>
+                </div>
+                <div class="content-box">
+                    <div class="header-title">
+                        <span>Work Experience</span>
+                    </div>
+                    <div class="box">
+                        <label>Company:</label>
+                        @foreach($template->user_work_histories as $work)
+                        <span>
+                            {!! $work['company'] !!}
+                        </span>,
+                        @endforeach
+                    </div>
+                </div>
+                <div class="content-box">
+                    <div class="header-title">
+                        <span>Skills</span>
+                    </div>
+                    <div class="box">
+                        @foreach($template->soft_skill as $skill)
+                        <span>
+                            {!! $skill['question'] !!}
+                        </span> ,
+                        @endforeach
+                        <p>
+                          <label for="amount">Text Size:</label>
+                        </p>
+                        <div id="slider-range-min"></div>
                     </div>
                 </div>
             </div>
