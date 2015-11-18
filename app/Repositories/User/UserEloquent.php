142964da074e1ff576b6bea096e7e0c0d2cbe223
nguyenphuocnhatthanh <nguyenphuocnhatthanh@gmail.com>
2015-11-18 10:22:45 +0700

    validate

--- app/Repositories/User/UserEloquent.php
+++ app/Repositories/User/UserEloquent.php
@@ -218,9 +218,9 @@ class UserEloquent extends AbstractRepository implements UserInterface
 	 */
 	public function editStatus($id, $status)
 	{
-		if ( !in_array($status, [1, 2, 3]))
+		if ( !in_array((int)$status, [1, 2, 3]))
 			return null;
-		
+
 		$user = $this->getById($id);
 		$user->status = $status;
 		$result = $user->save();
