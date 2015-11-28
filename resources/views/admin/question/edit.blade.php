08a04dd74604b8408c1e1b8039edb3156307bb3f
nguyenphuocnhatthanh <nguyenphuocnhatthanh@gmail.com>
2015-11-28 09:46:24 +0700

    complete admin manage question

--- resources/views/admin/question/edit.blade.php
+++ resources/views/admin/question/edit.blade.php
@@ -12,42 +12,63 @@
         <div class="form-group">
             <label for="content" class="col-sm-2 control-label">Content</label>
             <div class="col-sm-10">
-                <input type="text" value="{{ $question->content }}" class="form-control" id="content" placeholder="Content">
+                <input type="text" name="content" value="{{ $question->content }}" class="form-control" id="content" placeholder="Content">
             </div>
         </div>
-    </form>
-</div>
-<div class="modal-footer">
-    <button typ e="button" class="btn btn-default" data-dismiss="modal">Close</button>
-    <button type="button" class="btn btn-primary update">Save changes</button>
+        <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+                <button typ e="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                <button type="submit" class="btn btn-primary update">Save changes</button>
+            </div>
+        </div>
+       
+     </form>
 </div>
 
 <script>
-    $(document).ready(function() {
-        var isBusy = false;
-        $(document).off('click', 'button.update').on('click', 'button.update', function(e) {
-            e.preventDefault();
-            
-            if (isBusy) return;
+var isBusy = false;
+$('#form-update').validate({
+    rules: {
+        content: {required :true}
+    },
+    highlight: function(element) {
+        $(element).closest('.form-group').addClass('has-error');
+    },
+    unhighlight: function(element) {
+        $(element).closest('.form-group').removeClass('has-error');
+    },
+    errorElement: 'span',
+    errorClass: 'help-block',
+    errorPlacement: function(error, element) {
+        if(element.parent('.input-group').length) {
+            error.insertAfter(element.parent());
+        } else {
+            error.insertAfter(element);
+        }
+    },
+    submitHandler : function(form) {
+       event.preventDefault();
 
-            isBusy = true;
-            $.ajax({
-                url: $('#form-update').attr('action'),
-                type: 'POST',
-                data: {
-                    token: $('input[name=_token]').val(),
-                    id: $('input[name=id]').val(),
-                    content: $('#content').val()
-                },
-                success: function(result) {
-                    var message = result.status == true
-                        ? '<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Update successfully.</strong> </div>'
-                        : '<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Error when update!</strong></div>';
-                    $('#ajax-message').html(message);
-                }
-            }).always(function() {
-                isBusy = false;
-            });
+        if (isBusy) return;
+
+        isBusy = true;
+        $.ajax({
+            url: $('#form-update').attr('action'),
+            type: 'POST',
+            data: {
+                token: $('input[name=_token]').val(),
+                id: $('input[name=id]').val(),
+                content: $('#content').val()
+            },
+            success: function(result) {
+                var message = result.status == true
+                    ? '<div class="alert alert-success alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Update successfully.</strong> </div>'
+                    : '<div class="alert alert-danger alert-dismissible" role="alert"><button type="button" class="close" data-dismiss="alert" aria-label="Close"><span aria-hidden="true">&times;</span></button><strong>Error when update!</strong></div>';
+                $('#ajax-message').html(message);
+            }
+        }).always(function() {
+            isBusy = false;
         });
-    });
+    }
+ });
 </script>
