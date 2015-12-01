cfe6bdcf88881dce00f82ceddbd3caa691621985
nguyenphuocnhatthanh <nguyenphuocnhatthanh@gmail.com>
2015-12-01 15:25:43 +0700

    add rule validate register form

--- app/ValidatorApi/RegisterForm_Rule.php
+++ app/ValidatorApi/RegisterForm_Rule.php
@@ -6,8 +6,8 @@ class RegisterForm_Rule extends AbstractValidator
 	protected function rules($params)
 	{
 		return [
-            'firstname' => 'required|max:50',
-            'lastname' => 'required|max:45',
+            'firstname' => 'required|alpha|max:50',
+            'lastname' => 'required|alpha|max:45',
             'email' => 'required|email|unique:users|max:100',
             'password' => 'required|min:6',
         ];
