0dd7f8ac424c07283cb4befcdfcfc0808e2747a7
nguyenphuocnhatthanh <nguyenphuocnhatthanh@gmail.com>
2015-12-01 15:34:06 +0700

    add rules validation for AppServiceProvider

--- app/ValidatorApi/RegisterForm_Rule.php
+++ app/ValidatorApi/RegisterForm_Rule.php
@@ -6,8 +6,8 @@ class RegisterForm_Rule extends AbstractValidator
 	protected function rules($params)
 	{
 		return [
-            'firstname' => 'required|alpha|max:50',
-            'lastname' => 'required|alpha|max:45',
+            'firstname' => 'required|alpha_spaces|max:50',
+            'lastname' => 'required|alpha_spaces|max:45',
             'email' => 'required|email|unique:users|max:100',
             'password' => 'required|min:6',
         ];
