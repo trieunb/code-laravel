8dc422852fbc1c00316397e22a2340e99d474791
luanht <htluan2605@gmail.com>
2015-12-21 11:31:24 +0700

    use OpenSans font for admin dashboard

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -9,7 +9,8 @@
         <!-- Bootstrap CSS -->
         <link href="{{  asset('css/bootstrap.css') }}" rel="stylesheet">
         <link href="{{  asset('css/font-awesome.min.css') }}" rel="stylesheet">
-        
+        <link href="{{  asset('assets/css/OpenSans.css') }}" rel="stylesheet">
+
         <link rel="stylesheet" type="text/css" href="{{asset('assets/css/style.css')}}">
         <link rel="stylesheet" type="text/css" href="{{asset('assets/css/metisMenu.css')}}">
         <link rel="stylesheet" type="text/css" href="{{asset('assets/css/dataTables.bootstrap.css')}}">
@@ -37,6 +38,7 @@
                 <ul class="nav navbar-top-links navbar-right">
                     <li class="dropdown">
                         <a class="dropdown-toggle" data-toggle="dropdown" href="#">
+                            Admin
                             <i class="fa fa-user fa-fw"></i>  <i class="fa fa-caret-down"></i>
                         </a>
                         <ul class="dropdown-menu dropdown-user">
@@ -61,7 +63,7 @@
                                 <a href="{{URL::to('/admin')}}"><i class="fa fa-dashboard fa-fw"></i> Dashboard</a>
                             </li>
                             <li>
-                                <a href="{{ route('admin.user.get.index') }}"><i class="fa fa-user fa-fw"></i> User</a>
+                                <a href="{{ route('admin.user.get.index') }}"><i class="fa fa-user fa-fw"></i> User Management</a>
                             </li>
                             <li>
                                 <a href="#"><i class="fa fa-file-text fa-fw"></i> Market Place<span class="fa arrow"></span></a>
