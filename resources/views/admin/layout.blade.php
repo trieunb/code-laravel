923e0ec807138e2e6e823d6cbb1ed832bbe58d0d
luanht <htluan2605@gmail.com>
2015-12-24 16:17:24 +0700

    update layout for user details page

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -123,7 +123,7 @@
             <div id="page-wrapper">
                 <div class="row">
                     <div class="col-lg-12">
-                        <h1 class="page-header">@yield('page-header')</h1>
+                        <h3 class="page-header">@yield('page-header')</h3>
                     </div>
                     <!-- /.col-lg-12 -->
                 </div>
