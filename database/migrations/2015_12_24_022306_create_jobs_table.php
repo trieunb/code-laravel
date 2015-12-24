bb9bb0647cf1511b848d7b5a36102b9569a332ca
trieunb <trieunb08@gmail.com>
2015-12-24 09:48:32 +0700

    create table jobs anh categories

---
+++ database/migrations/2015_12_24_022306_create_jobs_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('jobs', function(Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('slug');
+            $table->string('company_name');
+            $table->string('skill');
+            $table->string('country');
+            $table->string('experience')->nullable();
+            $table->text('description')->nullable();
+            $table->string('salary')->nullable();
+            $table->string('logo')->nullable();
+            $table->timestamps()
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('jobs');
+    }
+}
---
+++ database/migrations/2015_12_24_024014_create_job_categories_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateJobCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('job_categories', function(Blueprint $table) {
+            $table->increments('id');
+            $table->integer('job_id');
+            $table->string('title');
+            $table->string('slug');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('job_categories');
+    }
+}
