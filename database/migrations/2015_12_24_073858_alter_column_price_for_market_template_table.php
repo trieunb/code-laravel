69748702713a0e5d0a6d11229969a7fe87556bd8
nguyenphuocnhatthanh <nguyenphuocnhatthanh@gmail.com>
2015-12-24 14:40:56 +0700

    correct price field market template table

---
+++ database/migrations/2015_12_24_073858_alter_column_price_for_market_template_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AlterColumnPriceForMarketTemplateTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('template_markets', function($table) {
+            $table->dropColumn('price');
+        });
+        Schema::table('users', function($table) {
+            $table->double('price', 15, 8)->nullable()->after('content');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
