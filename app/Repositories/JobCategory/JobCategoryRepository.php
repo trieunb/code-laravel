0c8795bf7e079b519ebfc854c17eba1635e8398b
nguyenphuocnhatthanh <nguyenphuocnhatthanh@gmail.com>
2015-12-28 14:16:09 +0700

    fix typo

--- app/Repositories/JobCategory/JobCategoryRepository.php
+++ app/Repositories/JobCategory/JobCategoryRepository.php
@@ -6,17 +6,17 @@ use App\Repositories\AbstractRepository;
 
 class JobCategoryRepository extends AbstractRepository
 {
-	private $model;
+    private $model;
 
-	public function __construct(JobCategory $model)
-	{
-		$this->model = $model;
-	}
+    public function __construct(JobCategory $model)
+    {
+        $this->model = $model;
+    }
 
-	public function getAll()
-	{
-		$job_categories = create_lists($this->model->select(['id', 'name', 'parent_id'])->get()->toArray());
+    public function getAll()
+    {
+        $job_categories = create_lists($this->model->select(['id', 'name', 'parent_id'])->get()->toArray());
 
         return sort_lists($job_categories);
-	}
+    }
 }
