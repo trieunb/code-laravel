64a93116e9879972cb413e29d7fc8a1d41095c61
nguyenphuocnhatthanh <nguyenphuocnhatthanh@gmail.com>
2015-12-31 14:30:02 +0700

    remove code

--- app/Http/ViewComposers/UserComposer.php
+++ app/Http/ViewComposers/UserComposer.php
@@ -2,21 +2,20 @@
 namespace App\Http\ViewComposers;
 
 use App\Models\User;
-use App\Repositories\User\UserInterface;
 use Illuminate\Contracts\View\View;
 
 class UserComposer
 {
     private $user;
 
-    public function __construct(UserInterface $user)
+    public function __construct(User $user)
     {
         $this->user = $user;
     }
 
     public function compose(View $view)
     {
-        $users = User::select('id', \DB::raw('CONCAT(firstname, " ", lastname) as name'))
+        $users = $this->user->select('id', \DB::raw('CONCAT(firstname, " ", lastname) as name'))
             ->lists('name', 'id');
 
         $view->with('list_users', $users->all());
